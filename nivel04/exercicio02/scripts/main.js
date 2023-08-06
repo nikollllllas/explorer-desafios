64ef460a9f2ce3bc315a0c208fd01ad111e17658
nikollllllas <nikollasohta2014@gmail.com>
2023-08-06 13:18:08 -0300

    :hammer: updated main.js

--- nivel04/exercicio02/scripts/main.js
+++ nivel04/exercicio02/scripts/main.js
@@ -1,59 +1,52 @@
-let numberOne = parseFloat(firstInput)
-let numberTwo = parseFloat(secondInput)
-
-function sum(numberOne, numberTwo) {
-    let sumResult = numberOne + numberTwo
-    return sumResult
-}
-
-function subtraction(numberOne, numberTwo) {
-    let subtractionResult = numberOne - numberTwo
-    return subtractionResult
-}
-
-function multiplication(numberOne, numberTwo) { 
-    let multiplicationResult = numberOne * numberTwo
-    return multiplicationResult
-}
-
-function division(numberOne, numberTwo) {
-    let divisionResult = numberOne / numberTwo
-    return divisionResult
-}
-
-function remainder(sumResult) {
-    if (sumResult % 2 == 0) {
-        var remainderResult = "A soma dos números é par"
-    } else {    
-        var remainderResult = "A soma dos números é ímpar"
+const students = [
+    {
+        name: 'Gracie',
+        grade: [
+            80,
+            90
+        ]
+    },
+    {
+        name: 'Paulo',
+        grade: [
+            100,
+            40
+        ]
+    },
+    {
+        name: 'Renato',
+        grade: [
+            70,
+            20
+        ]
+    },
+    {
+        name: 'Micaela',
+        grade: [
+            100,
+            97
+        ]
     }
-    return remainderResult
+]
+
+function gradeSum(students) {
+    const sums = students.map(student => student.grade[0] + student.grade[1]);
+    return sums;
 }
 
-function equal(numberOne, numberTwo) {
-    if (numberOne == numberTwo) {
-        var equalResult = "Os números são iguais"
+function getAverage(students) {
+    const sums = gradeSum(students);
+    const averages = sums.map(sum => sum / 2);
+    return averages;
+}
+
+averageResult = getAverage(students)
+
+for (i = 0; i < students.length; i++) {
+    if (averageResult[i] >=70) {
+        alert(`A média do(a) aluno(a) é: ${averageResult[i]} \nParabéns ${students[i].name}! Você foi aprovado(a)!`)
     } else {
-        var equalResult = "Os números são diferentes"
+        alert(`A média do(a) aluno(a) é: ${averageResult[i]} \nNão foi dessa vez ${students[i].name}... Tente novamente!`)
     }
-    return equalResult
 }
 
-let firstInput = prompt('Digite o primeiro número: ')
-let secondInput = prompt('Digite o segundo número: ')
-
-let sumResult = sum(numberOne, numberTwo)
-let subtractionResult = subtraction(numberOne, numberTwo)
-let multiplicationResult = multiplication(numberOne, numberTwo)
-let divisionResult = division(numberOne, numberTwo)
-let remainderResult = remainder(sumResult)
-let equalResult = equal(numberOne, numberTwo)
-
-
-
-alert(`O resultado da soma é: ${resultSoma}`)
-alert(`O resultado da subtração é: ${subtractionResult}`)
-alert(`O resultado da multiplicação é: ${multiplicationResult}`)
-alert(`O resultado da divisão é: ${divisionResult}`)
-alert(`${remainderResult}`)
-alert(`${equalResult}`)
