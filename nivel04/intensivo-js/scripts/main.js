673180e6e0d0d982b59894cf1d75c388c40a4ab0
nikollllllas <nikollasohta2014@gmail.com>
2023-08-15 11:54:48 -0300

    :hammer: updated main.js

--- nivel04/intensivo-js/scripts/main.js
+++ nivel04/intensivo-js/scripts/main.js
@@ -1,19 +1,19 @@
 // // 1.
-// alert('Hello World!')
+alert('Hello World!')
 
 
 // // 2.
-// let numberOne = 50
-// let numberTwo = 51
+let numberOne = 50
+let numberTwo = 51
 
-// function sumNumbers(numberOne, numberTwo) {
-//     const sum = numberOne + numberTwo
-//     return sum
-// }
+function sumNumbers(numberOne, numberTwo) {
+    const sum = numberOne + numberTwo
+    return sum
+}
 
-// sumResult = sumNumbers(numberOne, numberTwo)
+sumResult = sumNumbers(numberOne, numberTwo)
 
-// alert(`A soma dos dois números é: ${sumResult}`)
+alert(`A soma dos dois números é: ${sumResult}`)
 
 
 // 3. 
@@ -21,72 +21,72 @@ function numberGenerator () {
     const number = Math.floor(Math.random() * 100)
     return number
 }
-// let number = numberGenerator()
+let number = numberGenerator()
 
-// if (typeof number == 'number' ) {
-//     alert(`É um número`)
-// } else {
-//     alert(`Não é um número`)
-// }
+if (typeof number == 'number' ) {
+    alert(`É um número`)
+} else {
+    alert(`Não é um número`)
+}
 
 // 4.
-// const letter = 'Billy'
+const letter = 'Billy'
 
-// if (typeof letter == 'string') {
-//     alert('É uma string')
-// } else {
-//     alert('Não é uma string')
-// }
+if (typeof letter == 'string') {
+    alert('É uma string')
+} else {
+    alert('Não é uma string')
+}
 
-// 5.
-// const switcher = true
+5.
+const switcher = true
 
-// if (typeof switcher == 'boolean') {
-//     alert('É um booleano')
-// } else {
-//     alert('Não é um booleano')
-// }
+if (typeof switcher == 'boolean') {
+    alert('É um booleano')
+} else {
+    alert('Não é um booleano')
+}
 
 // 6.
-// const numberOne = numberGenerator()
-// const numberTwo = numberGenerator()
+const numberOneSub = numberGenerator()
+const numberTwoSub = numberGenerator()
 
-// function subtract(numberOne, numberTwo) {
-//     subtract = numberOne - numberTwo
-//     return subtract
-// }
+function subtract(numberOneSub, numberTwoSub) {
+    subtract = numberOneSub - numberTwoSub
+    return subtract
+}
 
-// const subtractResult = subtract(numberOne, numberTwo)
+const subtractResult = subtract(numberOne, numberTwo)
 
-// alert(`O resultado da subtração é: ${subtractResult}\nOs números utilizados foram: ${numberOne} e ${numberTwo}`)
+alert(`O resultado da subtração é: ${subtractResult}\nOs números utilizados foram: ${numberOne} e ${numberTwo}`)
 
 // 7.
 
-// const numberOne = numberGenerator()
-// const numberTwo = numberGenerator()
+const numberOneMulti = numberGenerator()
+const numberTwoMulti = numberGenerator()
 
-// function multiply (numberOne, numberTwo) {
-//     multiply = numberOne * numberTwo
-//     return multiply
-// }
+function multiplication (numberOneMulti, numberTwoMulti) {
+    multiplication = numberOneMulti * numberTwoMulti
+    return multiplication
+}
 
-// const multiplytResult = multiply(numberOne, numberTwo)
+const multiplytResult = multiply(numberOne, numberTwo)
 
-// alert(`O resultado da multiplicação é: ${multiplytResult}\nOs números utilizados foram: ${numberOne} e ${numberTwo}`)
+alert(`O resultado da multiplicação é: ${multiplytResult}\nOs números utilizados foram: ${numberOne} e ${numberTwo}`)
 
 // 8.
 
-// const numberOne = numberGenerator()
-// const numberTwo = numberGenerator()
+const numberOneDivi = numberGenerator()
+const numberTwoDivi = numberGenerator()
 
-// function division (numberOne, numberTwo) {
-//     division = numberOne / numberTwo
-//     return division
-// }
+function division (numberOneDivi, numberTwoDivi) {
+    division = numberOneDivi / numberTwoDivi
+    return division
+}
 
-// const divisionResult = division(numberOne, numberTwo)
+const divisionResult = division(numberOne, numberTwo)
 
-// alert(`O resultado da divisão é: ${divisionResult}\nOs número utilizados foram: ${numberOne} e ${numberTwo}`)
+alert(`O resultado da divisão é: ${divisionResult}\nOs número utilizados foram: ${numberOne} e ${numberTwo}`)
 
 // 9. e 10.
 const randomNumber = numberGenerator()
