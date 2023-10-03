8177ec75dea7b1c7077c608718c28b425948e2da
nikollllllas <nikollasohta2014@gmail.com>
2023-10-03 16:00:32 -0300

    :wrench: countdown logic

--- nivel05/focus-timer/js/FocusTimer/actions.js
+++ nivel05/focus-timer/js/FocusTimer/actions.js
@@ -1,13 +1,17 @@
 import state from "./state.js"
+import * as timer from './timer.js'
 
 export function toggleRunning() {
     state.isRunning = document.documentElement.classList.toggle('running')
+
+    timer.countdown()
 }
 
 export function reset() {
     state.isRunning = false
 
     document.documentElement.classList.toggle('running')
+    timer.updateDisplay()
 }
 
 export function set() {
