68f092e7bca8f109215b1f81a752601777e11bab
nikollllllas <nikollasohta2014@gmail.com>
2023-10-10 15:09:47 -0300

    :wrench: fetching infos from github api

--- nivel06/git-fav/js/Favorites.js
+++ nivel06/git-fav/js/Favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser {
+    static search(username) {
+        const endpoint = `https://api.github.com/users/${username}`	
+
+        return fetch(endpoint)
+            .then(response => response.json())
+            .then(( login, name, public_repos, followers ) =>({
+                login,
+                name,
+                public_repos,
+                followers,
+            }))
+    }
+}
+
 export class Favorites {
     constructor(root){
         this.root = document.querySelector(root)
