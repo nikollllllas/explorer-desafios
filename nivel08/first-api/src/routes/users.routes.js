0e19b58dc0e20c9f15b5361504f27b446c48e6da
nikollllllas <nikollasohta2014@gmail.com>
2023-11-09 14:11:56 -0300

    :wrench: remove middleware

--- nivel08/first-api/src/routes/users.routes.js
+++ nivel08/first-api/src/routes/users.routes.js
@@ -4,17 +4,8 @@ const usersRoutes = Router()
 
 const UsersController = require("../controllers/UsersController") 
 
-function myMiddleware(req, res, next) {
-  console.log("I'm a middleware")
-  if(req.body.isAdmin) {
-    return res.json({ error: "User unauthorized" })
-  }
-
-  next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
