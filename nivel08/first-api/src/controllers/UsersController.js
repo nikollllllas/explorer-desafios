b416fbd7947e3cf433df46f4c103c5792d6cda7f
nikollllllas <nikollasohta2014@gmail.com>
2023-11-13 00:57:57 -0300

    :wrench: update routes

--- nivel08/first-api/src/controllers/UsersController.js
+++ nivel08/first-api/src/controllers/UsersController.js
@@ -27,6 +27,47 @@ class UsersControllers {
 
     return res.status(201).json()
   }
+
+  async update(req, res) {
+    const { name, email, password, old_password} = req.body
+    const { id } = req.params
+
+    const database = await sqliteConnection()
+    const user = await database.get(`
+      SELECT * FROM users
+      WHERE id = (?)
+    `, [id])
+
+    if(!user) {
+      throw new AppError("User not found. :(")
+    }
+
+    const userWithUpdatedEmail = await database.get(`
+      SELECT * FROM users
+      WHERE email = (?)
+    `, [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Email already registered.")
+    }
+
+    user.name = name
+    user.email = email
+
+    if(password && !old_password) {`
+      throw new AppError("Old password is required.")
+      `
+    }
+
+    await database.run(`
+      UPDATE users SET
+        name = (?),
+        email = (?)
+        WHERE id = (?)
+      `, [user.name, user.email, id]) 
+
+      return res.json(user)
+  }
 }
 
 module.exports = UsersControllers
