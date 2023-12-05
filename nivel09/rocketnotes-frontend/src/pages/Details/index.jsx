0924d35df7a92103e53fe5264752c4ca29540ff8
nikollllllas <nikollasohta2014@gmail.com>
2023-12-05 15:29:11 -0300

    :wrench:: first component

--- nivel09/rocketnotes-frontend/src/pages/Details/index.jsx
+++ nivel09/rocketnotes-frontend/src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
 import { Container } from './styles'
+import { Button } from '../../components/button'
 
 export function Details() {
   return(
     <Container>
       <h1>Olá mundo</h1>
+
+      <Button />
     </Container>
   )
 }
