145d0a02aa5ff50aa0ecda7dcb32d69520b77a4d
nikollllllas <nikollasohta2014@gmail.com>
2023-12-13 20:02:46 -0300

    :wrench:: created SignUp page

---
+++ nivel09/rocketnotes-frontend/src/pages/SignUp/index.jsx
@@ -0,0 +1,41 @@
+import { BackgroundImage, Container, Form } from './styles'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { FiLock, FiMail, FiUser } from 'react-icons/fi'
+
+export function SignUp() {
+  return (
+    <Container>
+      <BackgroundImage />
+      <Form>
+        <h1>RocketNotes</h1>
+        <p>Aplicação para gerenciar e salvar seus links úteis.</p>
+
+        <h2>Crie sua conta</h2>
+        
+        <Input 
+          placeholder={'Nome'}
+          type={'text'}
+          icon={FiUser}
+          />
+
+        <Input 
+          placeholder={'E-mail'}
+          type={'text'}
+          icon={FiMail}
+          />
+        
+        <Input
+          placeholder={'Senha'}
+          type='password'
+          icon={FiLock}
+          />
+
+        <Button
+          title={'Cadastrar'} />
+
+        <a href='#'>Voltar para o Login</a>
+      </Form>
+    </Container>
+  )
+}
