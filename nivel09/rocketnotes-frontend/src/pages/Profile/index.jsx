fd22054ccce66d8ee6e77f98fcf35d0834d4e91e
nikollllllas <nikollasohta2014@gmail.com>
2023-12-14 18:20:37 -0300

    :wrench:: created Avatar component

--- nivel09/rocketnotes-frontend/src/pages/Profile/index.jsx
+++ nivel09/rocketnotes-frontend/src/pages/Profile/index.jsx
@@ -1,8 +1,8 @@
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-import { Container, Form } from './styles'
+import { Avatar, Container, Form } from './styles'
 
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 export function Profile() { 
   return (
@@ -13,6 +13,19 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img 
+            src='https://github.com/nikollllllas.png'
+            alt='Foto do Usuário'
+          />
+          <label htmlFor=''>
+            <FiCamera />
+            <input
+              id='avatar'
+              type='file'
+            />
+          </label>
+        </Avatar>
         <Input
           type='text'
           placeholder='Nome'
