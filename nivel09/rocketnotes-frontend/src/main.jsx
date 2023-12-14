d1be11c74907f5b44576d5c69d67b5bde7399a62
nikollllllas <nikollasohta2014@gmail.com>
2023-12-14 19:18:29 -0300

    :wrench:: created New page

--- nivel09/rocketnotes-frontend/src/main.jsx
+++ nivel09/rocketnotes-frontend/src/main.jsx
@@ -5,13 +5,13 @@ import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
