f2af9930021b9e5969562eecb02551609f1c659a
nikollllllas <nikollasohta2014@gmail.com>
2023-12-15 16:25:28 -0300

    :wrench:: created AppRoutes and AuthRoutes

--- nivel09/rocketnotes-frontend/src/main.jsx
+++ nivel09/rocketnotes-frontend/src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { Routes } from './routes'
+
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 
-import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
