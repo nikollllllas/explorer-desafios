382749124193b34a888f3ffef0c828dd959e538c
nikollllllas <nikollasohta2014@gmail.com>
2023-12-16 17:16:43 -0300

    :wrench:: add validation to user

--- nivel08/first-api/src/controllers/SessionsController.js
+++ nivel08/first-api/src/controllers/SessionsController.js
@@ -1,8 +1,17 @@
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
+
 class SessionsController {
   async create(req, res) {
     const { email, password } = req.body	
 
-    return res.json({ email, password })
+    const user = await knex('users').where({ email }).first()
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401)
+    }
+
+    return res.json(user)
   }
 }
 
