076d036ea00ef23d03fed6cc4cddee7a40053d36
nikollllllas <nikollasohta2014@gmail.com>
2023-12-19 14:50:03 -0300

    :wrench:: add Avatar route to upload profile pic

--- nivel08/first-api/src/routes/users.routes.js
+++ nivel08/first-api/src/routes/users.routes.js
@@ -1,4 +1,9 @@
 const { Router } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
+
+const upload = multer(uploadConfig.MULTER)
+
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router()
@@ -9,5 +14,6 @@ const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), usersController.updateAvatar )
 
 module.exports = usersRoutes
