94aa437294e20a87273bd0b05dc8e719e6830125
nikollllllas <nikollasohta2014@gmail.com>
2023-12-19 15:59:53 -0300

    :wrench:: add sign up functionality, form validation and connection with api

--- nivel09/rocketnotes-frontend/src/pages/SignUp/index.jsx
+++ nivel09/rocketnotes-frontend/src/pages/SignUp/index.jsx
@@ -1,10 +1,37 @@
-import { BackgroundImage, Container, Form } from './styles'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { api } from '../../services/api'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { Link, useNavigate } from 'react-router-dom'
 import { FiLock, FiMail, FiUser } from 'react-icons/fi'
+import { BackgroundImage, Container, Form } from './styles'
 
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert('Preencha todos os campos!')
+    }
+
+    api.post('/users', { name, email, password })
+    .then(() => {
+      alert('User created successfully!')
+      navigate('/')
+    })
+    .catch(e => {
+      if (e.res) {
+        alert(e.res.data.message)
+      } else {
+        alert('Error: User not created!')
+      }
+    })
+  }
+
   return (
     <Container>
       <BackgroundImage />
@@ -14,26 +41,31 @@ export function SignUp() {
 
         <h2>Crie sua conta</h2>
         
-        <Input 
+        <Input   
           placeholder={'Nome'}
           type={'text'}
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
           />
 
         <Input 
           placeholder={'E-mail'}
           type={'text'}
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
           />
         
         <Input
           placeholder={'Senha'}
           type='password'
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
           />
 
         <Button
-          title={'Cadastrar'} />
+          title={'Cadastrar'}
+          onChange={handleSignUp}
+          />
 
         <Link to='/'>Voltar para o Login</Link>
       </Form>
