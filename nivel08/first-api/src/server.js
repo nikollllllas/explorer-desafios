c8160be34b4b365753ca34fa246ed5a54ff65280
nikollllllas <nikollasohta2014@gmail.com>
2023-12-19 15:25:48 -0300

    :wrench:: finish file upload configuration

--- nivel08/first-api/src/server.js
+++ nivel08/first-api/src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-
+const uploadConfig = require('./configs/upload')
 const runMigrations = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 const express = require("express")
@@ -10,8 +10,9 @@ runMigrations()
 const app = express()
 app.use(express.json())
 
-app.use(routes)
+app.use('/files', express.static(uploadConfig))
 
+app.use(routes)
 
 app.use(( error, req, res, next ) => {
   if(error instanceof AppError) {
