1ba62e0019240e0d255ca610979d75dce2c1748f
nikollllllas <nikollasohta2014@gmail.com>
2023-12-19 15:09:10 -0300

    :wrench:: add UserAvatarController and update users.routes.js

--- nivel08/first-api/src/routes/users.routes.js
+++ nivel08/first-api/src/routes/users.routes.js
@@ -9,11 +9,12 @@ const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 const usersRoutes = Router()
 
 const UsersController = require('../controllers/UsersController') 
+const UserAvatarController = require('../controllers/UsersAvatarController')
 
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
 usersRoutes.put('/', ensureAuthenticated, usersController.update)
-usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), usersController.updateAvatar )
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), UserAvatarController.update)
 
 module.exports = usersRoutes
