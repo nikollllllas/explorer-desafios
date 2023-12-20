5ad954358f7e3e7141682d177cdcc5e501d8a213
nikollllllas <nikollasohta2014@gmail.com>
2023-12-20 11:12:28 -0300

    :wrench:: add auth to SignIn with session

--- nivel09/rocketnotes-frontend/src/hooks/auth.jsx
+++ nivel09/rocketnotes-frontend/src/hooks/auth.jsx
@@ -1,10 +1,32 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState } from 'react'
+import { api } from '../services/api'
 
-export const AuthContext = createContext({ })
+export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({})
+
+  async function signIn({ email, password }) {
+    try {
+      const res = await api.post('/sessions', {
+        email,
+        password
+      })
+      const { user, token } = res.data
+
+      api.defaults.headers.authorization = `Bearer ${token}`
+      setData({ user, token })
+    } catch (e) {
+      if (e.res) {
+        alert(e.res.data.message)
+      } else {
+        alert('Erro ao realizar o login')
+      }
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ name: 'nikollas', email: 'nikollas@email.com' }}>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
     </AuthContext.Provider>
   )
@@ -12,7 +34,7 @@ function AuthProvider({ children }) {
 
 function useAuth() {
   const context = useContext(AuthContext)
-  
+
   return context
 }
 
--- nivel09/rocketnotes-frontend/src/pages/SignIn/index.jsx
+++ nivel09/rocketnotes-frontend/src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../hooks/auth'
 import { Input } from '../../components/Input'
@@ -6,8 +7,17 @@ import { Button } from '../../components/Button'
 import { BackgroundImage, Container, Form } from './styles'
 
 export function SignIn() {
-  const data = useAuth()
-  console.log(data)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({
+      email,
+      password
+    })
+  }
 
   return (
     <Container>
@@ -16,21 +26,25 @@ export function SignIn() {
         <p>Aplicação para gerenciar e salvar seus links úteis.</p>
 
         <h2>Realize o Login</h2>
-        
-        <Input 
+
+        <Input
           placeholder={'E-mail'}
           type={'text'}
           icon={FiMail}
-          />
-        
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
         <Input
           placeholder={'Senha'}
           type='password'
           icon={FiLock}
-          />
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <Button
-          title={'Entrar'} />
+          title={'Entrar'}
+          onClick={handleSignIn}
+        />
 
         <Link to='/register'>Criar conta</Link>
       </Form>
