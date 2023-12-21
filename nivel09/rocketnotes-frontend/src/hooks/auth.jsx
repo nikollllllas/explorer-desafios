8fe150b71525f60cba6a23b65bda7a0ed39d8fef
nikollllllas <nikollasohta2014@gmail.com>
2023-12-21 19:22:54 -0300

    :bug:: updating req header

--- nivel09/rocketnotes-frontend/src/hooks/auth.jsx
+++ nivel09/rocketnotes-frontend/src/hooks/auth.jsx
@@ -17,7 +17,7 @@ function AuthProvider({ children }) {
       localStorage.setItem('@rocketnotes:user', JSON.stringify(user))
       localStorage.setItem('@rocketnotes:token', token)
 
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
       setData({ user, token })
     } catch (e) {
       if (e.res) {
@@ -40,7 +40,7 @@ function AuthProvider({ children }) {
     const token = localStorage.getItem('@rocketnotes:token')
 
     if (user && token) {
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
       setData({
         user: JSON.parse(user),
         token
