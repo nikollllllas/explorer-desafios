7bd411751b73a0eed70d56dac34e6304ed14fd2b
nikollllllas <nikollasohta2014@gmail.com>
2023-12-21 19:35:10 -0300

    :wrench: add states to profile page

--- nivel09/rocketnotes-frontend/src/pages/Profile/index.jsx
+++ nivel09/rocketnotes-frontend/src/pages/Profile/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { useAuth } from '../../hooks/auth'
 import { Link } from 'react-router-dom'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
@@ -6,6 +8,12 @@ import { Avatar, Container, Form } from './styles'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 export function Profile() {
+  const { user } = useAuth()
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [oldPassword, setOldPassword] = useState()
+  const [newPassword, setNewPassword] = useState()
+
   return (
     <Container>
       <header>
@@ -31,21 +39,27 @@ export function Profile() {
           type='text'
           placeholder='Nome'
           icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <Input
           type='text'
           placeholder='E-mail'
           icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <Input
           type='password'
           placeholder='Senha atual'
           icon={FiLock}
+          onChange={(e) => setOldPassword(e.target.value)}
         />
         <Input
           type='password'
           placeholder='Nova senha'
           icon={FiLock}
+          onChange={(e) => setNewPassword(e.target.value)}
         />
         <Button title='Salvar' />
       </Form>
