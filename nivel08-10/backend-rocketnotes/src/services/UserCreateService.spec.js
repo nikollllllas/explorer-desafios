0836fe9b65d8daa0e085dbcbb9600781a3d0929a
nikollllllas <nikollasohta2014@gmail.com>
2024-01-10 18:16:50 -0300

    :sparkles::: add describe function to the test

--- nivel08-10/backend-rocketnotes/src/services/UserCreateService.spec.js
+++ nivel08-10/backend-rocketnotes/src/services/UserCreateService.spec.js
@@ -1,19 +1,20 @@
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
 const UserCreateService = require('./UserCreateService')
 
-it('user should be created', async () => {
-  const user = {
-    name: 'User Test',
-    email: 'test@email.com',
-    password: '123321'
-  }
+describe('UserCreateService', () => {
+  it('user should be created', async () => {
+    const user = {
+      name: 'User Test',
+      email: 'test@email.com',
+      password: '123321'
+    }
 
-  const userRepositoryInMemory = new UserRepositoryInMemory()
-  const userCreateService = new UserCreateService(userRepositoryInMemory)
-  const userCreated = await userCreateService.execute(user)
+    const userRepositoryInMemory = new UserRepositoryInMemory()
+    const userCreateService = new UserCreateService(userRepositoryInMemory)
+    const userCreated = await userCreateService.execute(user)
 
-  console.log(userCreated)
+    console.log(userCreated)
 
-  expect(userCreated).toHaveProperty('id')
-
+    expect(userCreated).toHaveProperty('id')
+  })
 })
