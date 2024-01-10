162a7ce9985595beedb3522878c0f9eddc8afc6e
nikollllllas <nikollasohta2014@gmail.com>
2024-01-09 21:45:46 -0300

    :sparkles:: finished writing tests to UserCreate service

--- nivel08-10/backend-rocketnotes/src/services/UserCreateService.spec.js
+++ nivel08-10/backend-rocketnotes/src/services/UserCreateService.spec.js
@@ -1,14 +1,18 @@
+const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory')
 const UserCreateService = require('./UserCreateService')
 
-it('user should be created', () => {
+it('user should be created', async () => {
   const user = {
     name: 'User Test',
     email: 'test@email.com',
     password: '123321'
   }
 
-  const userCreateService = new UserCreateService()
+  const userRepositoryInMemory = new UserRepositoryInMemory()
+  const userCreateService = new UserCreateService(userRepositoryInMemory)
   const userCreated = await userCreateService.execute(user)
+
+  console.log(userCreated)
 
   expect(userCreated).toHaveProperty('id')
 
